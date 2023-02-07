d960f7b7cb03303e4e8f8e0b32727712ff2880a2
Наталья Арлазарова <sic15@yandex.ru>
2023-02-07 18:17:36 +0300

    Second try

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Определяет нанесенный урон."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f'{5 + randint(3, 5)}')
@@ -14,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Определяет блокированный урон."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +25,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Применяет специальные умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость 105»')
     if char_class == 'mage':
@@ -32,6 +35,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Вводная информация."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -56,6 +60,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
